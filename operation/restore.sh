6b795a4baa52b9ce2e7b54d591034feac1d93f41
Simon Gaus <phisto05@gmail.com>
2021-03-23 03:10:01 +0100

    Enhance error message

--- operation/restore.sh
+++ operation/restore.sh
@@ -16,27 +16,27 @@ fi
 
 # Check if the backup user exists
 #
-database_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.backup')")"
+database_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.backup');")"
 if [ "$database_user_exists" != 1 ]; then
-    echo "This restore script only works if the database was properly setup via the install script."
+    echo "This restore script only works if the database was properly setup via the install script. The backup user appears to be missing."
     sleep 1
     exit 1
 fi
 
 # Check if the api read user exists
 #
-reader_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.reader')")"
+reader_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.reader');")"
 if [ "$reader_user_exists" != 1 ]; then
-    echo "This restore script only works if the database was properly setup via the install script."
+    echo "This restore script only works if the database was properly setup via the install script. The api read user appears to be missing."
     sleep 1
     exit 1
 fi
 
 # Check if the api write user exists
 #
-writer_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.writer')")"
+writer_user_exists="$(mysql -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'festivals.api.writer');")"
 if [ "$writer_user_exists" != 1 ]; then
-    echo "This restore script only works if the database was properly setup via the install script."
+    echo "This restore script only works if the database was properly setup via the install script. The api write user appears to be missing."
     sleep 1
     exit 1
 fi
