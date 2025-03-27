7f083deae3326ddb339e187ebf4e522cd35c5ab7
Phisto <simon.cay.gaus@gmail.com>
2025-03-28 00:25:20 +0100

    Use config path

--- server/server.go
+++ server/server.go
@@ -66,7 +66,7 @@ func (s *Server) setMiddleware() {
 	// tell the ruter which middleware to use
 	s.Router.Use(
 		// used to log the request to the console | development
-		servertools.Middleware(servertools.TraceLogger("/var/log/festivals-database-node/trace.log")),
+		servertools.Middleware(servertools.TraceLogger(s.Config.TraceLog)),
 		// tries to recover after panics
 		middleware.Recoverer,
 	)
